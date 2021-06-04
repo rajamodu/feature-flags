ce13a9a7988d5dba2e4538764b0f80debee3040e
antonshell <antonshell@yandex.ru>
2021-06-04 08:57:01 +0300

    Implemented main controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+{
+    public function index(Request $request): Response
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+            'service' => 'feature flag service',
+        ]);
+    }
+}
