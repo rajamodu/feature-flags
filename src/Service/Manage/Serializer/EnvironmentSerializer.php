8ffecb1bbf7b2b383272ada687bcde4e1332533e
antonshell <antonshell@yandex.ru>
2021-07-13 11:52:13 +0300

    Updated EnvironmentSerializer and FeatureSerializer

--- src/Service/Manage/Serializer/EnvironmentSerializer.php
+++ src/Service/Manage/Serializer/EnvironmentSerializer.php
@@ -5,16 +5,44 @@ declare(strict_types=1);
 namespace App\Service\Manage\Serializer;
 
 use App\Entity\Environment;
+use App\Entity\Feature;
+use App\Entity\Project;
 use App\Service\AbstractSerializer;
 
 class EnvironmentSerializer extends AbstractSerializer
 {
+    public function serializeArray(array $data): array
+    {
+        $results = [];
+        foreach ($data as $environment) {
+            /** @var Environment $environment */
+            $results[] = [
+                'id' => $environment->getId(),
+                'name' => $environment->getName(),
+                'description' => $environment->getDescription(),
+            ];
+        }
+
+        return $results;
+    }
+
     public function serializeItem(Environment $environment): array
     {
         return [
             'id' => $environment->getId(),
             'name' => $environment->getName(),
             'description' => $environment->getDescription(),
+            'features' => $this->serializeFeatures($environment),
         ];
+    }
+
+    private function serializeFeatures(Environment $environment): array
+    {
+        $results = [];
+        foreach ($environment->getFeaturesValues() as $featureValue) {
+            $results[$featureValue->getFeature()->getName()] = $featureValue->isEnabled();
+        }
+
+        return $results;
     }
 }
--- src/Service/Manage/Serializer/FeatureSerializer.php
+++ src/Service/Manage/Serializer/FeatureSerializer.php
@@ -4,17 +4,44 @@ declare(strict_types=1);
 
 namespace App\Service\Manage\Serializer;
 
+use App\Entity\Environment;
 use App\Entity\Feature;
 use App\Service\AbstractSerializer;
 
 class FeatureSerializer extends AbstractSerializer
 {
+    public function serializeArray(array $data): array
+    {
+        $results = [];
+        foreach ($data as $feature) {
+            /** @var Feature $feature */
+            $results[] = [
+                'id' => $feature->getId(),
+                'name' => $feature->getName(),
+                'description' => $feature->getDescription(),
+            ];
+        }
+
+        return $results;
+    }
+
     public function serializeItem(Feature $feature): array
     {
         return [
             'id' => $feature->getId(),
             'name' => $feature->getName(),
             'description' => $feature->getDescription(),
+            'values' => $this->serializeValues($feature),
         ];
+    }
+
+    private function serializeValues(Feature $feature): array
+    {
+        $results = [];
+        foreach ($feature->getValues() as $featureValue) {
+            $results[$featureValue->getEnvironment()->getName()] = $featureValue->isEnabled();
+        }
+
+        return $results;
     }
 }
