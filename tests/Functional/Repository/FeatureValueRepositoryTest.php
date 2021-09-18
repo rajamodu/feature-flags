4e948e8ab7872d55183470d7460484cc680b85cf
antonshell <antonshell@yandex.ru>
2021-09-18 08:55:24 +0300

    Implemented FeatureValueRepositoryTest

--- tests/Functional/Repository/FeatureValueRepositoryTest.php
+++ tests/Functional/Repository/FeatureValueRepositoryTest.php
@@ -4,20 +4,56 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Repository;
 
-use App\Tests\DataFixtures\EmptyFixture;
+use App\Entity\Environment;
+use App\Entity\Feature;
+use App\Entity\FeatureValue;
+use App\Repository\FeatureValueRepository;
+use App\Tests\DataFixtures\Repository\FeatureRepositoryFixture;
+use App\Tests\DataFixtures\Repository\FeatureValueRepositoryFixture;
 use App\Tests\Functional\FixtureWebTestCase;
 
 class FeatureValueRepositoryTest extends FixtureWebTestCase
 {
+    /**
+     * @var FeatureValueRepository
+     */
+    private $featureValueRepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::bootKernel();
+        $container = self::$kernel->getContainer();
+        $this->featureValueRepository = $container->get('doctrine')->getRepository(FeatureValue::class);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->featureValueRepository);
+
+        parent::tearDown();
+    }
+
     public function testFindOneByFeatureAndEnvironment(): void
     {
+        /** @var Feature $feature */
+        $feature = $this->getReference(FeatureRepositoryFixture::DEMO_FEATURE1_REF);
 
+        /** @var Environment $environment */
+        $environment = $this->getReference(FeatureRepositoryFixture::DEMO_ENV_PROD_REF);
+
+        $featureValue = $this->featureValueRepository->findOneByFeatureAndEnvironment($feature, $environment);
+
+        self::assertFalse($featureValue->isEnabled());
+        self::assertEquals($feature, $featureValue->getFeature());
+        self::assertEquals($environment, $featureValue->getEnvironment());
     }
 
     protected function getFixtures(): array
     {
         return [
-            EmptyFixture::class,
+            FeatureValueRepositoryFixture::class,
         ];
     }
 }
