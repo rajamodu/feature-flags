e444ee2c84bf24acd3d7a21594a651e111bb3d86
antonshell <antonshell@yandex.ru>
2021-09-24 19:03:41 +0300

    Implemented AuthServiceTest. Removed unused test classes

--- tests/Unit/Service/AuthServiceTest.php
+++ tests/Unit/Service/AuthServiceTest.php
@@ -4,27 +4,48 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Service;
 
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use App\Service\AuthService;
 use PHPUnit\Framework\TestCase;
 
 class AuthServiceTest extends TestCase
 {
-    public function testGetProjectByManageKey(): void
-    {
+    private AuthService $authService;
+    private ProjectRepository $projectRepository;
 
+    protected function setUp(): void
+    {
+        $this->projectRepository
+            = $this->getMockBuilder(ProjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $this->authService = new AuthService(
+            $this->projectRepository,
+            ''
+        );
     }
 
-    public function testVerifyReadAccessToken(): void
+    protected function tearDown(): void
     {
-
+        unset(
+            $this->authService,
+            $this->projectRepository
+        );
     }
 
-    public function testGetTokenFromGlobals(): void
+    public function testVerifyReadAccessTokenMissing(): void
     {
-
+        unset($_SERVER['HTTP_AUTHORIZATION']);
+        $project = new Project();
+        self::assertFalse($this->authService->verifyReadAccessToken($project));
     }
 
-    public function testValidateRootToken(): void
+    public function testGetTokenFromGlobalsMissing(): void
     {
-
+        unset($_SERVER['HTTP_AUTHORIZATION']);
+        self::assertNull($this->authService->getTokenFromGlobals());
     }
 }
